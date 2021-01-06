c6910009c2a0b9f0cf6ab0393fdcc8fc620ac144
ArildoJunior <arildoajunior@gmail.com>
2021-01-06 14:48:54 -0300

    Moving pieces

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -4,7 +4,6 @@ public class BoardException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public BoardException(String msg) {
-		super (msg);
+		super(msg);
 	}
-
 }
--- src/chess/ChessExcepition.java
+++
@@ -1,9 +0,0 @@
-package chess;
-
-public class ChessExcepition extends RuntimeException{
-		private static final long serialVersionUID = 1L;
-
-		public ChessExcepition(String msg) {
-			super(msg);
-		}
-}
