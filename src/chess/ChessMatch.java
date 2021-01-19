0c2e758226029ca3afef2c0aeb94738ebf89abf5
ArildoJunior <arildoajunior@gmail.com>
2021-01-19 10:32:21 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -192,29 +193,32 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		 placeNewPiece('a', 1, new Rook(board, Color.White));
-	        placeNewPiece('e', 1, new King(board, Color.White));
-	        placeNewPiece('h', 1, new Rook(board, Color.White));
-	        placeNewPiece('a', 2, new Pawn(board, Color.White));
-	        placeNewPiece('b', 2, new Pawn(board, Color.White));
-	        placeNewPiece('c', 2, new Pawn(board, Color.White));
-	        placeNewPiece('d', 2, new Pawn(board, Color.White));
-	        placeNewPiece('e', 2, new Pawn(board, Color.White));
-	        placeNewPiece('f', 2, new Pawn(board, Color.White));
-	        placeNewPiece('g', 2, new Pawn(board, Color.White));
-	        placeNewPiece('h', 2, new Pawn(board, Color.White));
+		placeNewPiece('a', 1, new Rook(board, Color.White));
+		placeNewPiece('c', 1, new Bishop(board, Color.White));
+	    placeNewPiece('e', 1, new King(board, Color.White));
+	    placeNewPiece('f', 1, new Bishop(board, Color.White));
+	    placeNewPiece('h', 1, new Rook(board, Color.White));
+	    placeNewPiece('a', 2, new Pawn(board, Color.White));
+	    placeNewPiece('b', 2, new Pawn(board, Color.White));
+	    placeNewPiece('c', 2, new Pawn(board, Color.White));
+	    placeNewPiece('d', 2, new Pawn(board, Color.White));
+	    placeNewPiece('e', 2, new Pawn(board, Color.White));
+	    placeNewPiece('f', 2, new Pawn(board, Color.White));
+	    placeNewPiece('g', 2, new Pawn(board, Color.White));
+	    placeNewPiece('h', 2, new Pawn(board, Color.White));
 
-	        placeNewPiece('a', 8, new Rook(board, Color.Black));
-	        placeNewPiece('e', 8, new King(board, Color.Black));
-	        placeNewPiece('h', 8, new Rook(board, Color.Black));
-	        placeNewPiece('a', 7, new Pawn(board, Color.Black));
-	        placeNewPiece('b', 7, new Pawn(board, Color.Black));
-	        placeNewPiece('c', 7, new Pawn(board, Color.Black));
-	        placeNewPiece('d', 7, new Pawn(board, Color.Black));
-	        placeNewPiece('e', 7, new Pawn(board, Color.Black));
-	        placeNewPiece('f', 7, new Pawn(board, Color.Black));
-	        placeNewPiece('g', 7, new Pawn(board, Color.Black));
-	        placeNewPiece('h', 7, new Pawn(board, Color.Black));
-		
+	    placeNewPiece('a', 8, new Rook(board, Color.Black));
+	    placeNewPiece('c', 8, new Bishop(board, Color.Black));
+	    placeNewPiece('e', 8, new King(board, Color.Black));
+	    placeNewPiece('f', 8, new Bishop(board, Color.Black));
+	    placeNewPiece('h', 8, new Rook(board, Color.Black));
+	    placeNewPiece('a', 7, new Pawn(board, Color.Black));
+	    placeNewPiece('b', 7, new Pawn(board, Color.Black));
+	    placeNewPiece('c', 7, new Pawn(board, Color.Black));
+	    placeNewPiece('d', 7, new Pawn(board, Color.Black));
+	    placeNewPiece('e', 7, new Pawn(board, Color.Black));
+	    placeNewPiece('f', 7, new Pawn(board, Color.Black));
+	    placeNewPiece('g', 7, new Pawn(board, Color.Black));
+	    placeNewPiece('h', 7, new Pawn(board, Color.Black));
 	}
 }
