28ad1b39aa3735e63686a884882e5da7ace99e62
ArildoJunior <arildoajunior@gmail.com>
2021-01-19 10:14:38 -0300

    Pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -191,12 +192,29 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		placeNewPiece('h', 7, new Rook(board, Color.White));
-		placeNewPiece('d', 1, new Rook(board, Color.White));
-		placeNewPiece('e', 1, new King(board, Color.White));
-		
-		placeNewPiece('b', 8, new Rook(board, Color.Black));
-		placeNewPiece('a', 8, new King(board, Color.Black));
+		 placeNewPiece('a', 1, new Rook(board, Color.White));
+	        placeNewPiece('e', 1, new King(board, Color.White));
+	        placeNewPiece('h', 1, new Rook(board, Color.White));
+	        placeNewPiece('a', 2, new Pawn(board, Color.White));
+	        placeNewPiece('b', 2, new Pawn(board, Color.White));
+	        placeNewPiece('c', 2, new Pawn(board, Color.White));
+	        placeNewPiece('d', 2, new Pawn(board, Color.White));
+	        placeNewPiece('e', 2, new Pawn(board, Color.White));
+	        placeNewPiece('f', 2, new Pawn(board, Color.White));
+	        placeNewPiece('g', 2, new Pawn(board, Color.White));
+	        placeNewPiece('h', 2, new Pawn(board, Color.White));
+
+	        placeNewPiece('a', 8, new Rook(board, Color.Black));
+	        placeNewPiece('e', 8, new King(board, Color.Black));
+	        placeNewPiece('h', 8, new Rook(board, Color.Black));
+	        placeNewPiece('a', 7, new Pawn(board, Color.Black));
+	        placeNewPiece('b', 7, new Pawn(board, Color.Black));
+	        placeNewPiece('c', 7, new Pawn(board, Color.Black));
+	        placeNewPiece('d', 7, new Pawn(board, Color.Black));
+	        placeNewPiece('e', 7, new Pawn(board, Color.Black));
+	        placeNewPiece('f', 7, new Pawn(board, Color.Black));
+	        placeNewPiece('g', 7, new Pawn(board, Color.Black));
+	        placeNewPiece('h', 7, new Pawn(board, Color.Black));
 		
 	}
 }
